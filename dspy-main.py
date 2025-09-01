89685185291f406ad10a47bf6b0cae94be3bdeac
Da Def <da.def.lang.belta@gmail.com>
2025-09-01 17:18:03 +0200

    .

---
+++ dspy-main.py
@@ -0,0 +1,19 @@
+import dspy
+import os
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage: {} target task".format(sys.argv[0],))
+    sys.exit()
+
+target = open(sys.argv[1])
+task   = open(sys.argv[2])
+
+lm = dspy.LM("cerebras/qwen-3-coder-480b",
+        api_key=os.environ['CEREBRAS_API_KEY'])
+        #api_base="")
+dspy.configure(lm=lm)
+
+prog = dspy.Predict("target, task-> program")
+
+print(prog(target=target.read(), task=task.read()))
